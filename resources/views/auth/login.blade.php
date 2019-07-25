23f16417bbed61dd705888d931de8d7302b44375
Salma Salsabilla <salmasalsabilla42@gmail.com>
2019-07-25 18:58:12 +0700

    ALHAMDULILLAH YA ALLAH KELAR!!!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,49 +26,111 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+        }
+        body{
+            background-image: url('https://images.unsplash.com/photo-1526554850534-7c78330d5f90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80');
+            background-size: cover;
+            background-attachment: fixed;
+            font-family: new time roman;
+        }
+        h1{
+            font-size: 40px;
+            color: black;
+            margin-top: 250px;
+        }
+        p{
+            font-size: 17px;
+            color: black;
+        }
+        h3{
+            font-size: 25px;
+            color: white;
+        }
+        .glyphicon-log-in
+        {
+            font-size: 50px;
+            color: white;
+            float: right;
+            margin-top: 20px;
+        }
+        .col-md-5{
+            margin-top:80px;
+            box-shadow: -1px 1px 60px 10px white;
+            background: rgba(0,0,0,0.4);
+        }
+        .label{
+            font-weight: normal;
+            margin-top: 15px;
+            color: white;
+            font-size: 18px;
+        }
+        .form-control{
+            background: transparent;
+            border-radius: 0px;
+            border: 0px;
+            border-bottom: 1px solid white;
+            font-size: 18px;
+            margin-top: 15px;
+            height: 40px;
+            color: white;
+        }
+        .btn-info{
+            margin-top: 20px;
+            font-size: 20px;
+            width: 120px;
+            margin-left: 80px;
+            margin-bottom: 20px;
+        }
+        .tengah{
+            position: absolute;
+            top: 20%;
+            left: 30%
+        }
+    </style>
 </head>
-<body class="hold-transition login-page" >
-<div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Sistem </b>Arsip</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="login" method="post">
-        {{ csrf_field() }}
-      <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email" name="email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" name="password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-4">
-          
+<body>
+    <div class="tengah">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="text-left">Login Now!</h3>
+                    </div>
+                    <div class="col-md-6">
+                        <span class="glyphicon glyphicon-log-in"></span>
+                    </div>
+                </div>
+                    <hr>
+                    <form action="login" method="post">
+                    {{ csrf_field() }}
+                        <div class="row">
+                            <label class="label col-md-2 control-label">E-mail</label>
+                            <div class="col-md-10">
+                                <input type="email" class="form-control" placeholder="Input Email" name="email" >
+                            </div>
+                        </div>
+                        <div class="row">
+                            <label class="label col-md-2 control-label">Password</label>
+                            <div class="col-md-10">
+                                <input type="password" class="form-control" placeholder="Input Password" name="password" >
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-info " style="margin-left: 40%;">Sign In</button>
+                    </form>
+                  <a href="{{route('register')}}" class="text-center" style="margin-left: 35%; color:white;">Register a new membership</a>
+                </div>
+            </div>
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-    <br>
-    <a href="{{route('register')}}" class="text-center">Register a new membership</a>
-
-  </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
-
-
-
-<!-- jQuery 3 -->
-<script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    </div>
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- iCheck -->
